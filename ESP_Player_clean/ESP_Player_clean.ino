5ae1ab85c35f636e86adeb6f93dcb82a310a483d
mooha <51880853+dkm1978@users.noreply.github.com>
2020-12-24 09:23:48 +0100

    Add files via upload

---
+++ ESP_Player_clean/ESP_Player_clean.ino
@@ -0,0 +1,49 @@
+
+#include "hxcmod.h"
+#include "enigma.h"
+
+
+
+
+#define DAC1 25
+#define DAC2 26
+
+volatile  uint16_t buff[2] ;
+
+
+
+
+modcontext mcontext;
+void* tune=(void*) &big;
+hw_timer_t * timer = NULL;
+
+
+void IRAM_ATTR onTimer() {
+  
+      hxcmod_fillbuffer(&mcontext,(msample*) &buff,1,0 );
+      dacWrite(DAC1,buff[0]);
+    
+}
+
+
+void setup() {
+  
+  
+  hxcmod_init( &mcontext );
+  hxcmod_setcfg( &mcontext, 44100, 1, 0);
+  hxcmod_load( &mcontext,tune, sizeof(big) );
+  timer = timerBegin(0, 5, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 363, true);
+  timerAlarmEnable(timer);
+  
+ 
+  
+}
+
+
+void loop() {
+      
+  
+                    
+}
